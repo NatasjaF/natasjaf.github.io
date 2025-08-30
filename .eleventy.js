a10828cdd99696f727cd0c4c70a8d82685c917d2
NatasjaF <n.frilingou@gmail.com>
2025-08-30 15:29:26 +0300

    Add Nunjucks date filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = function(eleventyConfig) {
+  // 🔽 add this block
+  eleventyConfig.addNunjucksFilter("date", (value, format = "yyyy") => {
+    const d = (!value || value === "now") ? new Date() : new Date(value);
+    if (format === "yyyy") return String(d.getFullYear());
+    return d.toISOString();
+  });
+
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy({ "src/cv.pdf": "cv.pdf" });
 
